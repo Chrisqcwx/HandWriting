9da58e208c9265dc5e2df141aa5f653a6ab4ae23
Chris <3075401625@qq.com>
2023-04-09 15:00:24 +0800

    refactor: remove some unused comment

--- handwriting.py
+++ handwriting.py
@@ -7,7 +7,7 @@ import config
 def get_pos(i, j, bias=4):
     return 70 + random.random() * bias / 2 + 25 * j, 83 + random.random() * bias + i * 48
 
-def draw_word(txt_path='./test.txt', ttf_path="./src/font.TTF", save_path="./result/", size=4, num_cols=1, title=None):
+def draw_word(txt_path='./test.txt', ttf_path="./src/font.TTF", save_path="./result/", offset_size=4, num_cols=1, title=None):
     font = ImageFont.truetype(ttf_path, 25)
     with open(txt_path, 'r', encoding='utf-8') as f:
         string = f.read()
@@ -25,7 +25,7 @@ def draw_word(txt_path='./test.txt', ttf_path="./src/font.TTF", save_path="./res
                 title_len = len(title)
                 start_idx = 19-title_len//2
                 for j, ch in enumerate(title):
-                    draw.text(get_pos(0, j+start_idx), ch, (0,0,0), font=font)
+                    draw.text(get_pos(0, j+start_idx, offset_size), ch, (0,0,0), font=font)
 
             for num_col in range(num_cols):
                 if idx >= length:
@@ -48,7 +48,7 @@ def draw_word(txt_path='./test.txt', ttf_path="./src/font.TTF", save_path="./res
                             else:
                                 break
                         i, j = row, num_col * (cols+1) + col
-                        pos = get_pos(i, j, size)
+                        pos = get_pos(i, j, offset_size)
                         draw.text(pos, ch, (0,0,0), font=font)
             if title is None:
                 img.save(save_path + str(page)+'.png')
@@ -58,12 +58,9 @@ def draw_word(txt_path='./test.txt', ttf_path="./src/font.TTF", save_path="./res
 
 
 if __name__ == "__main__":
-    size = config.offset_size  # 整齐度
-    # txt_path = './text/fyfn.txt'  # 文档位置
-    # ttf_path = "src/test.TTF"  # 字体位置
-    # save_path = "./result/"  # 储存文件夹 若没有不会自动生成
+    offset_size = config.offset_size  # 随机便宜量
     txt_path = config.text_dir + config.text_name
     ttf_path = config.ttf_path
     save_path = config.result_dir
-    draw_word(txt_path, ttf_path, save_path, size, num_cols=config.cols, title=config.title_name)
+    draw_word(txt_path, ttf_path, save_path,offset_size, num_cols=config.cols, title=config.title_name)
     print("success!")
